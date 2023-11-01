48542943e4305d85d9012d6aa7f08d48ef12e911
raghib1992 <raghib.npti@gmail.com>
2023-11-02 00:43:37 +0530

    add scripting for edit xml file

---
+++ Powershell_scripting/Sample_script/CreateNewFile.ps1
@@ -0,0 +1,10 @@
+﻿Push-Location -Path U:\test-script
+Get-ChildItem
+mkdir -Path test-script
+# Create New File
+New-Item -Path .\File.txt -ItemType File -Force
+Get-ChildItem
+# Add Content into file
+$data = "Mango is my favourite fruits"
+Add-Content -Path File.txt -Value $data -Force
+Get-Content -Path File.txt
---
+++ Powershell_scripting/Sample_script/EditXmlFile.ps1
@@ -0,0 +1,30 @@
+﻿$xmlFilePath = "C:\Users\raghi\Desktop\web.txt"
+
+[xml]$xmlData = Get-Content -Path $xmlFilePath
+
+$AppData = $xmlData.configuration.appSettings.add
+
+
+#$AppData.add.value
+
+foreach($url in $AppData)
+{
+    if($url.key -like "mic_service_url"){
+        if($url.value -like "https://wss.qa.paygateway.com/mic"){
+            Write-Host "No need to update url"
+            $url.value
+            #$url.value = "https://wss.qa.paygateway.com/mic"
+        }
+        else{
+            Write-Host "url value need to update"
+            Write-Host "https://wss.qa.paygateway.com/mic"
+            $url.value = "https://wss.qa.paygateway.com/mic"
+            $xmlData.save($xmlFilePath)
+        }
+    }
+    #$url.key
+    #$url.value
+
+    #Write-Host "Key is ($url.key) and Value is ($url.value)" 
+}
+
